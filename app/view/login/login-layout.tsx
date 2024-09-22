614979f08c8e36f00a29ab56434dd291f79164d7
Jeongsohee <toy3045@gmail.com>
2024-09-23 03:35:35 +0900

    Fix: #15 이미지 priority 속성 추가
    - Largest Contentful Paint(LCP) 성능 지표에 대한 최적화를 권장하는 경고 발생으로 priority 속성 추가하여 해결

--- app/view/login/login-layout.tsx
+++ app/view/login/login-layout.tsx
@@ -3,24 +3,31 @@ import Image from "next/image";
 export default function LoginLayout() {
   return (
     <div className="w-full h-screen flex flex-col justify-between p-20 bg-indigo-500">
-      <Image
-        src="/logo.png"
-        width={80}
-        height={80}
-        className="hidden md:block"
-        alt="develog logo"
-      />
+      <div className="hidden md:block relative w-[80px] h-[80px]">
+        <Image
+          src="/logo.png"
+          fill
+          sizes="80px"
+          style={{ objectFit: "contain" }}
+          alt="develog logo"
+          priority
+        />
+      </div>
+
       <h3 className="text-white text-4xl leading-relaxed">
         Let's optimize
         <br /> our team's work
       </h3>
-      <Image
-        src="/illust.png"
-        width={400}
-        height={400}
-        className="hidden md:block"
-        alt="develog logo"
-      />
+      <div className="hidden md:block relative w-full max-w-[400px] h-[400px]">
+        <Image
+          src="/illust.png"
+          fill
+          sizes="(max-width: 768px) 100vw, 400px"
+          style={{ objectFit: "contain" }}
+          alt="Illustration"
+          priority
+        />
+      </div>
     </div>
   );
 }
