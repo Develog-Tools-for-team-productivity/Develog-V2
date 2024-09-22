e31628cfca0e21d90ef77866e177ec86813cf4e9
Jeongsohee <toy3045@gmail.com>
2024-09-22 23:35:32 +0900

    Feat: #4 깃 권한 스코프 Organizations 추가
    - GithubProvider에 authorization scope read:org 추가

--- auth.ts
+++ auth.ts
@@ -7,6 +7,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+      authorization: {
+        params: {
+          scope: "read:user user:email repo read:org",
+        },
+      },
     }),
   ],
   pages: {
